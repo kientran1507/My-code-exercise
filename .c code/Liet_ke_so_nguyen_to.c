a21323537a611c0f2000ffad703a00193071346c
Tran Trung Kien <kelirious@gmail.com>
2023-10-17 13:04:17 +0700

    Liet ke cac so nguyen to su dung de quy

---
+++ .c code/Liet_ke_so_nguyen_to.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int n, int divisor) {
+    if (n <= 1) {
+        return false;
+    }
+    if (divisor == 1) {
+        return true;
+    }
+    if (n % divisor == 0) {
+        return false;
+    }
+    return isPrime(n, divisor - 1);
+}
+
+void listPrimes(int n) {
+    if (n >= 2) {
+        listPrimes(n - 1);
+        if (isPrime(n, n - 1)) {
+            printf("%d ", n);
+        }
+    }
+}
+
+int main() {
+    int limit = 50; // Thay đổi giới hạn tùy ý
+    printf("Cac so nguyen to nho hon hoac bang %d la: \n", limit);
+    listPrimes(limit);
+    printf("\n");
+
+    return 0;
+}
