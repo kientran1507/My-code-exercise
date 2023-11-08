c4bbae2016d4f8917785ab5dabd7d2ae0e02b628
Tran Trung Kien <kelirious@gmail.com>
2023-11-08 23:44:24 +0700

    Fixing a small bug with the addStudent func and adding a student_list.txt for reading and saving file

--- .c code/Student list.c
+++ .c code/Student list.c
@@ -14,36 +14,11 @@ struct Student {
 struct Student* head = NULL;
 struct Student* tail = NULL;
 
-// Hàm để đọc danh sách sinh viên từ tệp văn bản
-void loadStudentList() {
-    FILE* file = fopen("student_list.txt", "r");
-    if (file == NULL) {
-        printf("Error opening the file for reading.\n");
-        return;
-    }
-
-    char line[256];
-    char name[100];
-    char email[100];
-
-    while (fgets(line, sizeof(line), file) != NULL) {
-        if (strstr(line, "Name: ") != NULL) {
-            sscanf(line, "Name: %99[^\n]", name);
-        } else if (strstr(line, "Email: ") != NULL) {
-            sscanf(line, "Email: %99[^\n]", email);
-            addStudent(name, email);
-        }
-    }
-
-    fclose(file);
-    printf("Student list loaded from student_list.txt\n");
-}
-
 // Hàm để in ra danh sách sinh viên
 void printStudentList() {
     struct Student* current = head;
     while (current != NULL) {
-        printf("Name: %s\n", current->name);
+        printf("Student Name: %s\n", current->name);
         printf("Email: %s\n", current->email);
         printf("\n");
         current = current->next;
@@ -92,7 +67,52 @@ void deleteStudent(char name[]) {
         }
         current = current->next;
     }
-    printf("Student with name %s not found.\n", name);
+    printf("Student with name %s not found.\n");
+}
+
+// Hàm để tìm kiếm hồ sơ sinh viên theo tên hoặc email
+void searchStudent(char query[]) {
+    struct Student* current = head;
+    int found = 0;
+
+    while (current != NULL) {
+        if (strstr(current->name, query) != NULL || strstr(current->email, query) != NULL) {
+            printf("Student Name: %s\n", current->name);
+            printf("Email: %s\n", current->email);
+            printf("\n");
+            found = 1;
+        }
+        current = current->next;
+    }
+
+    if (!found) {
+        printf("No matching student found for '%s'.\n", query);
+    }
+}
+
+// Hàm để đọc danh sách sinh viên từ tệp văn bản
+void loadStudentList() {
+    FILE* file = fopen("student_list.txt", "r");
+    if (file == NULL) {
+        printf("Error opening the file for reading.\n");
+        return;
+    }
+
+    char line[256];
+    char name[100];
+    char email[100];
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        if (strstr(line, "Student Name: ") != NULL) {
+            sscanf(line, "Student Name: %99[^\n]", name);
+        } else if (strstr(line, "Email: ") != NULL) {
+            sscanf(line, "Email: %99[^\n]", email);
+            addStudent(name, email);
+        }
+    }
+
+    fclose(file);
+    printf("Student list loaded from student_list.txt\n");
 }
 
 // Hàm để lưu danh sách sinh viên vào một file văn bản
@@ -105,7 +125,7 @@ void saveStudentList() {
 
     struct Student* current = head;
     while (current != NULL) {
-        fprintf(file, "Name: %s\n", current->name);
+        fprintf(file, "Student Name: %s\n", current->name);
         fprintf(file, "Email: %s\n\n", current->email);
         current = current->next;
     }
@@ -116,6 +136,7 @@ void saveStudentList() {
 
 int main() {
     int choice;
+    char query[100];
     char name[100];
     char email[100];
 
@@ -125,10 +146,14 @@ int main() {
         printf("2. Print Student List\n");
         printf("3. Add Student\n");
         printf("4. Delete Student\n");
-        printf("5. Save Student List\n");
-        printf("6. Quit\n");
+        printf("5. Search Student by Name or Email\n");
+        printf("6. Save Student List\n");
+        printf("7. Quit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
+
+        // Clear the input buffer
+        while (getchar() != '\n');
 
         switch (choice) {
             case 1:
@@ -140,20 +165,29 @@ int main() {
                 break;
             case 3:
                 printf("Enter student name: ");
-                scanf("%s", name);
+                fgets(name, sizeof(name), stdin);
+                name[strcspn(name, "\n")] = 0;  // Remove the newline character
                 printf("Enter student email: ");
-                scanf("%s", email);
+                fgets(email, sizeof(email), stdin);
+                email[strcspn(email, "\n")] = 0;  // Remove the newline character
                 addStudent(name, email);
                 break;
             case 4:
                 printf("Enter student name to delete: ");
-                scanf("%s", name);
+                fgets(name, sizeof(name), stdin);
+                name[strcspn(name, "\n")] = 0;  // Remove the newline character
                 deleteStudent(name);
                 break;
             case 5:
-                saveStudentList();
+                printf("Enter student name or email to search: ");
+                fgets(query, sizeof(query), stdin);
+                query[strcspn(query, "\n")] = 0;  // Remove the newline character
+                searchStudent(query);
                 break;
             case 6:
+                saveStudentList();
+                break;
+            case 7:
                 exit(0);
             default:
                 printf("Invalid choice. Please try again.\n");
