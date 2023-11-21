c12c03acafa52b290f22143de3ece377b133d321
kientran1507 <133241619+kientran1507@users.noreply.github.com>
2023-11-21 19:13:08 +0700

    To hop chap k cua n

--- .c code/To hop chap k cua n (co nho).c
+++ .c code/To hop chap k cua n (co nho).c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX 100
+#define MAX 1000
+#define MOD 1000000007
 
-int M[MAX][MAX];
-int C(int k, int n){
-    if (k==0 || k==n) M[k][n]=1;
-    else if(M[k][n]==0){
-        M[k][n] = C(k, n-1) + C(k-1, n-1);
+long long M[MAX][MAX];
+
+long long C(long long k, long long n){
+    if (k == 0 || k == n) {
+        M[k][n] = 1;
+    } else if (M[k][n] == 0) {
+        M[k][n] = (C(k, n-1) + C(k-1, n-1)) % MOD;
     }
     return M[k][n];
 }
+
 int main(){
+    long long k, n;
     memset(M, 0, sizeof(M));
-    printf("%d", C(5,5));
+
+    scanf("%lld %lld", &k, &n);
+    printf("%lld\n", C(k, n));
+
+    return 0;
 }
