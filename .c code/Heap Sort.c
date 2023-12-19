8a8b357a4817362616e330a2b4e021ea9528eb56
kientran1507 <133241619+kientran1507@users.noreply.github.com>
2023-12-19 15:37:28 +0700

    Heap Sort

---
+++ .c code/Heap Sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(int A[], int i, int N){
+    int L = 2*i;
+    int R = 2*i + 1;
+    int max = i;
+    if (L <= N && A[L] > A[i]) max = L;
+    if (R <= N && A[R] > A[max]) max = R;
+    if (max != i){
+        swap(&A[i], &A[max]);
+        heapify(A, max, N);
+    }
+}
+
+void buildHeap(int A[], int N){
+    for(int i = N/2; i >= 1; i--){
+        heapify(A, i, N);
+    }
+}
+
+void heapSort(int A[], int N){
+    // index tu 1 -> N
+    buildHeap(A, N);
+    for (int i = N; i > 1; i--){
+        swap(&A[1], &A[i]);
+        heapify(A, 1, i-1);
+    }
+}
+
+int main(void){
+    int n;
+    int A[100006];
+    
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++){
+        scanf("%d", &A[i]);
+    }
+
+    heapSort(A, n);
+
+    for (int i = 1; i <= n; i++){
+        printf("%d ", A[i]);
+    }
+
+    return 0;
+}
