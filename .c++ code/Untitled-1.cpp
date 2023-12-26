e7323dd70949c785c3fe2d46b8c2b6b5a84ed8ff
kientran1507 <133241619+kientran1507@users.noreply.github.com>
2023-12-26 21:55:21 +0700

    Small changes

--- .c++ code/Untitled-1.cpp
+++ .c++ code/Untitled-1.cpp
@@ -1,73 +1,52 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-const int N = 1e6+1;
-int n;
-long long a[N];
-long long S0[N];
-long long S1[N];
-bool B0[N];
-bool B1[N];
+const int N = 1e3;
+const int INF = 1e9;
+int R, C;
+int cost[N][N];
+int min(int x, int y, int z);
 
-void input(){
-    cin >> n;
-    for (int i = 1; i <= n; i++){
-        cin >> a[i];
+int minCost(int cost[N][N], int m, int n)
+{
+	int i, j;
+
+	int tc[R][C];
+
+	tc[0][0] = cost[0][0];
+
+	for (i = 1; i <= m; i++){
+		tc[i][0] = tc[i - 1][0] + cost[i][0];
     }
-}
 
-void solve(){
-    if (a[1] % 2 == 0){
-        S0[1] = a[1];
-        B0[1] = true;
-        B1[1] = false;
-    } else {
-        S1[1] = a[1];
-        B0[1] = false;
-        B1[1] = true;
-    } 
-    for (int i = 2; i <= n; i++){
-        if (a[i] % 2 == 0){
-            if (B0[i-1]){
-                if (S0[i-1] > 0) S0[i] = S0[i-1] + a[i];
-                else S0[i] = a[i];
-                B0[i] = true;
-            } else {
-                S0[i] = a[i];
-                B0[i] = true;
-            }
-            if (B1[i-1]){
-                S1[i] = S1[i-1] + a[i];
-                B1[i] = true;
-            } else {
-                B1[i] = false;
-            }
-        } else {
-            if (B1[i-1]){
-                S0[i] = S1[i-1] + a[i];
-                B0[i] = true;
-            } else {
-                B0[i] = false;
-            }
-            if (B0[i-1]){
-                if (S0[i-1] > 0) S1[i] = S0[i-1] + a[i];
-                else S1[i] = a[i];
-                B1[i] = true;
-            } else {
-                S1[i] = a[i];
-                B1[i] = true;
-            }
+	for (j = 1; j <= n; j++){
+		tc[0][j] = tc[0][j - 1] + cost[0][j];
+    }
+
+	for (i = 1; i <= m; i++){
+		for (j = 1; j <= n; j++){
+			tc[i][j] = min(tc[i - 1][j - 1], tc[i - 1][j], tc[i][j - 1]) + cost[i][j];
         }
     }
-    long long ans = -1e18;
-    for (int i = 1; i <= n; i++){
-        if (B0[i] && ans < S0[i]) ans = S0[i];
-    }
-    cout << ans << endl;
+
+	return tc[m][n];
 }
 
-int main(void){
-    input();
-    solve();
-    return 0;
+int min(int x, int y, int z){
+	return min(x, min(y, z));
 }
+
+int main()
+{
+	cin >> R >> C;
+	for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+			cin >> cost[i][j];
+			if (cost[i][j] == 0) cost[i][j] = INF;
+		}
+	}
+	cout << minCost(cost, R-1, C-1);
+	return 0;
+}
+
